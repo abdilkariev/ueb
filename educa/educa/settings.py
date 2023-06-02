2d7f9e137dbd5025572774bf5dc8137fe5e08635
abdilkariev <abdilkarievv@gmail.com>
2023-06-02 09:05:35 +0300

    first

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -8,6 +8,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse_lazy
+
+
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'embed_video',
+    'students.apps.StudentsConfig',
     'courses.apps.CoursesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
